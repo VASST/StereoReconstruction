c2bdc0cc713f7a7795ac85282b84430d1f012a8f
Uditha <ujayarat@robarts.ca>
2016-12-22 16:14:40 -0800

    Adding functionality to save output as a video.

--- Codes/Sub-Label_Stereo/main.m
+++ Codes/Sub-Label_Stereo/main.m
@@ -4,8 +4,6 @@ addpath ./miccai2013/
 addpath('C:\Libs\build\prost\matlab\Release')
 addpath('C:\Libs\src\prost\matlab')
 
-
-
 % clear screen 
 clc
 close all
@@ -25,6 +23,8 @@ end
 
 % Matching method
 method = 'miccai2013';
+enable_comparison_with_ground_truth = false;
+enable_saving_output_to_video = false;
 
 
 if(gpu_enable)
@@ -36,7 +36,7 @@ if(gpu_enable)
 %     left_img = im2single(rgb2gray(imread(left_img_file)));
 %     right_img = im2single(rgb2gray(imread(right_img_file)));
     
-    % Read-in video
+    % Read-in video and stereo parameters
     left_frames = read_video_file('./images/f7_dynamic_deint_L.avi');
     right_frames = read_video_file('./images/f7_dynamic_deint_R.avi');
     ground_truth_prefix = './images/f7_dynamic_deint/disparityMap_';
@@ -70,107 +70,129 @@ if(gpu_enable)
                                     right_cam_params,...
                                     stereo_cam_rot, ...
                                     stereo_cam_trans');   
-                                
-                                
-    frame_no = 10;
-    I1 = im2single(rgb2gray(left_frames(:,:,:,frame_no)));
-    I2 = im2single(rgb2gray((right_frames(:,:,:,frame_no))));
     
-    % Rectify images
-    disp('Rectifying image...');
-    [left_img, right_img] = rectifyStereoImages(I1, I2, stereo_params, 'OutputView', 'valid');
-    figure, imshowpair(left_img, right_img, 'montage');
-    title('Input Images')
-    figure, imshowpair(left_img, right_img, 'falsecolor', 'ColorChannels', 'red-cyan');
-                                  
-    
-    width = size(left_img, 2);
-    height = size(left_img, 1);
-    true_disparity = read_ground_truth_disparity([ground_truth_prefix, num2str(frame_no-1),...
-                                                    '.txt'], ...
-                                                      size(I1,2), size(I1,1));
-                                                  
-        
-    % Cost volume parameters
-    CostVolumeParams = struct('min_disp', uint8(0), ...
-                             'max_disp', uint8(64), ...
-                             'method', 'zncc', ...
-                             'win_r', uint8(4), ...
-                             'ref_left', true);
-                         
-if(strcmp(method, 'miccai2013'))
-%% Chang et. al, MICCAI 2013
-    disp('Running Chang et. al., MICCAI 2013..')
-    s = sprintf('Size of the cost volume %dx%dx%d', size(left_img, 1), ...
-                                                        size(left_img, 2), ...
-                                                          CostVolumeParams.max_disp - CostVolumeParams.min_disp);
-    disp(s);
-
-    [disparity, disparity_color, err] = StereoReconstHuberL1(left_img, right_img, CostVolumeParams);
-    
-    % Plot results
-    figure;
-    imshow(mat2gray(disparity));
-    
-    figure, imshow(disparity_color);  
-    
-    figure, imshow(mat2gray(true_disparity))
-    title('Truth')    
-        
-    figure;
-    plot(err, 'g');    
-    grid on;
-    legend('HuberL1+Cost-Volume');
-    xlabel('Iterations');
-    ylabel('Energy function');
-    
-    % Reconstruct the 3D-world coordinates
-    xyzPoints = reconstructScene(disparity, stereo_params);
-    pcshow(xyzPoints);
-
-elseif(strcmp(method, 'sublabel_lifting')) 
-%% Functional Lifting
-
-    if(exist('cost_volume.mat'))
-        load('cost_volume.mat');
-        
-        
-    if ~exist('stereo_cost')            
-    %% compute matching cost
-    ndisps = 64;
-    stereo_cost = compute_stereo_cost(...
-        'images/im-dL.png', ...
-        'images/im-dR.png', ndisps, 1, 1);
+    if enable_saving_output_to_video
+        writerObj = VideoWriter('./disparity_output_miccai2013.avi');
+        writerObj.FrameRate = 25;
+        open(writerObj);
     end
-     
-        %% setup parameters
-        L = 32;
-        gamma = linspace(0, 1, L)'; 
-        lmb = 0.3;
+                                
+    %% Start Experiment
+    for frame_no = 10:10
+                                
+        I1 = im2single(rgb2gray(left_frames(:,:,:,frame_no)));
+        I2 = im2single(rgb2gray((right_frames(:,:,:,frame_no))));
 
-        %% solve problem and display result
-        tic;
-        [u_unlifted] = sublabel_lifting_convex(double(cost_volume), gamma, lmb);
-        toc;
+        % Rectify images
+        disp('Rectifying image...');
+        [left_img, right_img] = rectifyStereoImages(I1, I2, stereo_params, 'OutputView', 'valid');
+        figure, imshowpair(left_img, right_img, 'montage');
+        title('Input Images')
+        figure, imshowpair(left_img, right_img, 'falsecolor', 'ColorChannels', 'red-cyan');
 
-        [ny, nx, ~] = size(cost_volume);  
-        u_unlifted = min(max(u_unlifted, 0), 1);
-        num_colors = 65536;
-        cmap = jet(num_colors);
-        cmap_index = 1 + round(u_unlifted * (num_colors - 1));
-        image_rgb = reshape(cmap(cmap_index,:),ny,nx,3);
-        imshow(image_rgb)
+
+        width = size(left_img, 2);
+        height = size(left_img, 1);
         
-%         cmap_index = 1 + round(true_disparity*(num_colors-1));
-%         true_map = reshape(cmap(cmap_index,:),ny,nx,3);
-%         imshowpair(true_disparity, reshape(u_unlifted, ny, nx), 'montage');       
-        
-        
-    else
-        disp('Cost volume does not exist');
+        if enable_comparison_with_ground_truth
+             true_disparity = read_ground_truth_disparity([ground_truth_prefix, num2str(frame_no-1),...
+                                                        '.txt'], ...
+                                                          size(I1,2), size(I1,1));
+        end
+
+
+        % Cost volume parameters
+        CostVolumeParams = struct('min_disp', uint8(0), ...
+                                 'max_disp', uint8(64), ...
+                                 'method', 'zncc', ...
+                                 'win_r', uint8(4), ...
+                                 'ref_left', true);
+
+        if(strcmp(method, 'miccai2013'))
+        %% Chang et. al, MICCAI 2013
+            disp('Running Chang et. al., MICCAI 2013..')
+            s = sprintf('Size of the cost volume %dx%dx%d', size(left_img, 1), ...
+                                                                size(left_img, 2), ...
+                                                                  CostVolumeParams.max_disp - CostVolumeParams.min_disp);
+            disp(s);
+
+            [disparity, disparity_color, err] = StereoReconstHuberL1(left_img, right_img, CostVolumeParams);
+
+            % Plot results
+            figure;
+            imshow(mat2gray(disparity));
+
+            figure, imshow(disparity_color);  
+
+            if enable_comparison_with_ground_truth
+                figure, imshow(mat2gray(true_disparity))
+                title('Truth')    
+            end
+
+            figure;
+            plot(err, 'g');    
+            grid on;
+            legend('HuberL1+Cost-Volume');
+            xlabel('Iterations');
+            ylabel('Energy function');
+
+            % Reconstruct the 3D-world coordinates
+            xyzPoints = reconstructScene(disparity, stereo_params);
+            pc_handle = pcshow(xyzPoints);
+            
+            if enable_saving_output_to_video
+                f = getframe(gcf);
+                writeVideo(writerObj, f);   
+            end
+            
+        elseif(strcmp(method, 'sublabel_lifting')) 
+        %% Functional Lifting
+
+            if(exist('cost_volume.mat'))
+                load('cost_volume.mat');
+
+
+            if ~exist('stereo_cost')            
+            %% compute matching cost
+            ndisps = 64;
+            stereo_cost = compute_stereo_cost(...
+                'images/im-dL.png', ...
+                'images/im-dR.png', ndisps, 1, 1);
+            end
+
+                %% setup parameters
+                L = 32;
+                gamma = linspace(0, 1, L)'; 
+                lmb = 0.3;
+
+                %% solve problem and display result
+                tic;
+                [u_unlifted] = sublabel_lifting_convex(double(cost_volume), gamma, lmb);
+                toc;
+
+                [ny, nx, ~] = size(cost_volume);  
+                u_unlifted = min(max(u_unlifted, 0), 1);
+                num_colors = 65536;
+                cmap = jet(num_colors);
+                cmap_index = 1 + round(u_unlifted * (num_colors - 1));
+                image_rgb = reshape(cmap(cmap_index,:),ny,nx,3);
+                imshow(image_rgb)
+
+        %         cmap_index = 1 + round(true_disparity*(num_colors-1));
+        %         true_map = reshape(cmap(cmap_index,:),ny,nx,3);
+        %         imshowpair(true_disparity, reshape(u_unlifted, ny, nx), 'montage');       
+
+
+            else
+                disp('Cost volume does not exist');
+            end
+        end
     end
-end
     
+    if enable_saving_output_to_video
+        % Close writer object
+        close(writerObj);
+    end
 
 %% Release GPU memory
     reset(gpu);
